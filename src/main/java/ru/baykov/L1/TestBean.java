7dbf5c7cb75c70a33ef505e1228905a2025f7376
victor <vic.baykov89@gmail.com>
2023-09-30 19:18:30 +0300

    L1 - Первое приложение Spring Core

---
+++ src/main/java/ru/baykov/L1/TestBean.java
@@ -0,0 +1,18 @@
+package ru.baykov.L1;
+
+public class TestBean {
+    private String name;
+
+    public TestBean(String name) {
+        this.name = name;
+        System.out.println("===>Bean Created<===");
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/main/java/ru/baykov/L1/TestSpring.java
@@ -0,0 +1,12 @@
+package ru.baykov.L1;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class TestSpring {
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
+        TestBean bean = ctx.getBean("testBean", TestBean.class);
+        System.out.println("===>" + bean.getName() + "<===");
+        ctx.close();
+    }
+}
