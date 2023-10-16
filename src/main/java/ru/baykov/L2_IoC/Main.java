bed331587b9259ded9c7b5101d0f181f3b6fb34a
victor <vic.baykov89@gmail.com>
2023-10-16 17:40:42 +0300

    L4 - DI. Внедрение зависимостей через applicationContext с использованием геттеров

--- src/main/java/ru/baykov/L2_IoC/Main.java
+++ src/main/java/ru/baykov/L2_IoC/Main.java
@@ -4,9 +4,11 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
     public static void main(String[] args) {
-        ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContextL2.xml");
+        ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContextL3.xml");
         MusicPlayer musicPlayer = ctx.getBean("musicPlayer", MusicPlayer.class);
         musicPlayer.playMusic();
+        System.out.println(musicPlayer.getName());
+        System.out.println(musicPlayer.getVolume());
         ctx.close();
     }
 }
